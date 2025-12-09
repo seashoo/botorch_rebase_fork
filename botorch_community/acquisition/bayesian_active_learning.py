190ca97145a202990a36b7e829a72847af88e5d7
Carl Hvarfner <hvarfner@meta.com>
2025-12-09 12:29:02 -0800

    Fixing compute_tuning_factor in HIPE (#3108)
    Summary:
    Pull Request resolved: https://github.com/meta-pytorch/botorch/pull/3108
    
    Added the acquisition function to compute the beta tuning factor, which was previously computed incorrectly in the OSS version.
    
    Reviewed By: dme65
    
    Differential Revision: D88658257
    
    fbshipit-source-id: 86afe54f0ff9ccf886324b93dd092c98ba888fa7

--- botorch_community/acquisition/bayesian_active_learning.py
+++ botorch_community/acquisition/bayesian_active_learning.py
@@ -35,8 +35,6 @@ from __future__ import annotations
 
 import math
 
-from typing import Literal
-
 import torch
 from botorch.acquisition.acquisition import MCSamplerMixin
 from botorch.acquisition.bayesian_active_learning import (
@@ -44,16 +42,19 @@ from botorch.acquisition.bayesian_active_learning import (
     qBayesianActiveLearningByDisagreement,
 )
 from botorch.acquisition.objective import PosteriorTransform
-from botorch.models.fully_bayesian import MCMC_DIM, SaasFullyBayesianSingleTaskGP
-from botorch.optim import optimize_acqf
+from botorch.models.fully_bayesian import (
+    AbstractFullyBayesianSingleTaskGP,
+    MCMC_DIM,
+    SaasFullyBayesianSingleTaskGP,
+)
 from botorch.sampling.base import MCSampler
+from botorch.sampling.normal import SobolQMCNormalSampler
 from botorch.utils.sampling import draw_sobol_samples
 from botorch.utils.transforms import (
     average_over_ensemble_models,
     concatenate_pending_points,
     t_batch_mode_transform,
 )
-
 from botorch_community.utils.stat_dist import mvn_hellinger_distance, mvn_kl_divergence
 from torch import Tensor
 
@@ -239,35 +240,128 @@ class qExpectedPredictiveInformationGain(FullyBayesianAcquisitionFunction):
         return (prev_entropy - post_entropy).mean(-1)
 
 
+class qConditionalHyperparameterInformationGain(
+    FullyBayesianAcquisitionFunction, MCSamplerMixin
+):
+    def __init__(
+        self,
+        model: AbstractFullyBayesianSingleTaskGP,
+        mc_points: Tensor,
+        X_pending: Tensor | None = None,
+        sampler: MCSampler | None = None,
+        posterior_transform: PosteriorTransform | None = None,
+    ) -> None:
+        """Conditional hyperparameter information gain for active learning.
+
+        Computes the mutual information between mc_points and the
+        hyperparameters of the model, conditioned on the candidates currently under
+        evaluation. Used as a helper to compute beta in
+        qHyperparameterInformedPredictiveExploration, but does not have empirically
+        demonstrated stand-alone utility. The calculation of the acquisition function
+        is very similar to qBayesianActiveLearningByDisagreement, but is computed on
+        the mc_points, with the model conditioned on the points under consideration.
+
+        Args:
+            model: A fully bayesian model (FullyBayesianSingleTaskGP).
+            mc_points: A `N x d` tensor of points to use for MC-integrating the
+                posterior entropy (test set). Usually, these are qMC samples on
+                the whole design space.
+            X_pending: A `m x d`-dim Tensor of `m` design points.
+            sampler: The sampler used for drawing samples to approximate the entropy
+                of the Gaussian Mixture posterior.
+            posterior_transform: A PosteriorTransform. If provided, the posterior
+                obtained from the model will be transformed before computing the
+                acquisition value.
+        """
+        super().__init__(model=model)
+        if sampler is None:
+            sampler = SobolQMCNormalSampler(sample_shape=torch.Size([128]))
+        MCSamplerMixin.__init__(self, sampler=sampler)
+        self.mc_points = mc_points
+        self.set_X_pending(X_pending)
+        self.posterior_transform = posterior_transform
+
+        # Calling once to be able to condition on observations later
+        model.posterior(model.train_inputs[0])
+
+    @concatenate_pending_points
+    @t_batch_mode_transform()
+    @average_over_ensemble_models
+    def forward(self, X: Tensor) -> Tensor:
+        # Start by Conditioning on the candidate point X.
+        # NOTE: Multi-task GPs are not supported. This is a limitation
+        # in the current implementation of `condition_on_observations`.
+        posterior = self.model.posterior(
+            X, observation_noise=True, posterior_transform=self.posterior_transform
+        )
+        noise = (
+            posterior.variance
+            - self.model.posterior(
+                X,
+                observation_noise=False,
+                posterior_transform=self.posterior_transform,
+            ).variance
+        )
+
+        # The posterior entropy is independent of the actual outcome Y, so we can
+        # simply condition on a dummy observation (e.g. the posterior mean).
+        cond_Y = posterior.mean
+        cond_X = X.unsqueeze(-3).expand(*[cond_Y.shape[:-1] + X.shape[-1:]])
+        conditional_model = self.model.condition_on_observations(
+            X=cond_X,
+            Y=cond_Y,
+            noise=noise,
+        )
+
+        # This should not be computed as an entropy over one multivariate normal,
+        # but the expectation of entropies over individual gaussians. Thus, we
+        # unsqueeze to ensure that this happens.
+        conditional_posterior = conditional_model.posterior(
+            self.mc_points.unsqueeze(-2).unsqueeze(-2), observation_noise=True
+        )
+        samples = self.get_posterior_samples(conditional_posterior)
+        prev_samples = samples.unsqueeze(0).transpose(0, MCMC_DIM).squeeze(-1)
+        component_sample_probs = conditional_posterior.mvn.log_prob(prev_samples).exp()
+
+        # average over mixture components
+        mixture_sample_probs = component_sample_probs.mean(dim=-1, keepdim=True)
+
+        # this is the average over the model and sample dim
+        prev_entropy = -mixture_sample_probs.log().mean(dim=[0, 1])
+
+        # the posterior entropy is an average entropy over gaussians, so no mixture
+        post_entropy = -conditional_posterior.mvn.log_prob(samples.squeeze(-1)).mean(0)
+        hyperparameter_info_gain = (prev_entropy - post_entropy).mean(0)
+        return hyperparameter_info_gain
+
+
 class qHyperparameterInformedPredictiveExploration(
     FullyBayesianAcquisitionFunction, MCSamplerMixin
 ):
     def __init__(
         self,
-        model: SaasFullyBayesianSingleTaskGP,
+        model: AbstractFullyBayesianSingleTaskGP,
         mc_points: Tensor,
         bounds: Tensor,
         sampler: MCSampler | None = None,
         posterior_transform: PosteriorTransform | None = None,
         X_pending: Tensor | None = None,
-        num_samples: int = 512,
-        beta: float | None = None,
-        beta_tuning_method: Literal["sobol", "optimize"] = "sobol",
+        beta_tuning_samples: int = 32,
     ) -> None:
         """Hyperparameter-informed Predictive Exploration acquisition function.
 
         This acquisition function combines the mutual information between the
-        subsequent queries and a test set (predictive information gain) with the
+        subsequent queries and a test set (Expected Predictive Information Gain) with
         mutual information between observations and hyperparameters (BALD), weighted
         by a tuning factor. This balances exploration of the design space with
         reduction of hyperparameter uncertainty.
 
         The acquisition function is computed as:
             beta * BALD + TSIG
-        where beta is either provided or automatically tuned.
+        where beta is automatically tuned.
 
         Args:
-            model: A fully bayesian model (SaasFullyBayesianSingleTaskGP).
+            model: A fully bayesian model (FullyBayesianSingleTaskGP).
             mc_points: A `N x d` tensor of points to use for MC-integrating the
                 posterior entropy (test set). Usually, these are qMC samples on
                 the whole design space.
@@ -280,12 +374,10 @@ class qHyperparameterInformedPredictiveExploration(
                 acquisition value.
             X_pending: A `m x d`-dim Tensor of `m` design points that have been
                 submitted for evaluation but have not yet been observed.
-            num_samples: Number of samples to use for MC estimation of entropy.
-            beta: Fixed tuning factor. If None, it will be automatically computed
-                on the first forward pass based on the batch size q.
-            beta_tuning_method: Method for tuning beta. Options are "optimize"
-                (optimize acquisition function to find beta) or "sobol" (use sobol
-                samples). Only used when beta is None.
+            beta_tuning_samples: The number of sobol samples to use for tuning
+                beta. Does not use proper acquisition function optimization due to
+                a memory leak in settings.propagate_grads, outlined here:
+                (https://github.com/meta-pytorch/botorch/issues/2728)
         """
         if mc_points.ndim != 2:
             raise ValueError(
@@ -293,47 +385,40 @@ class qHyperparameterInformedPredictiveExploration(
                 f"{mc_points.shape}"
             )
         super().__init__(model=model)
-        MCSamplerMixin.__init__(self)
+        MCSamplerMixin.__init__(self, sampler=sampler)
         self.set_X_pending(X_pending)
-        self.num_samples = num_samples
-        self.beta_tuning_method = beta_tuning_method
         self.register_buffer("mc_points", mc_points)
         self.register_buffer("bounds", bounds)
         self.sampler = sampler
         self.posterior_transform = posterior_transform
-        self._tuning_factor: float | None = beta
-        self._tuning_factor_q: int | None = None
+        self.num_beta_tuning_samples = beta_tuning_samples
+
+        self._tuning_factor = None
+        self._tuning_factor_q = 0
 
     def _compute_tuning_factor(self, q: int) -> None:
         """Compute the tuning factor beta for weighting BALD vs TSIG."""
-        if self.beta_tuning_method == "sobol":
-            draws = draw_sobol_samples(
-                bounds=self.bounds,
-                q=q,
-                n=1,
-            ).squeeze(0)
-            # Compute the ratio at sobol samples
-            tsig_val = qExpectedPredictiveInformationGain.forward(
-                self,
-                draws,
-            )
-            bald_val = qBayesianActiveLearningByDisagreement.forward(self, draws)
-            self._tuning_factor = (tsig_val / (bald_val + 1e-8)).mean().item()
-        elif self.beta_tuning_method == "optimize":
-            # Optimize to find the best tuning factor
-            bald_acqf = qBayesianActiveLearningByDisagreement(
-                model=self.model,
-                sampler=self.sampler,
-            )
-            _, bald_val = optimize_acqf(
-                bald_acqf,
-                bounds=self.bounds,
-                q=q,
-                num_restarts=1,
-                raw_samples=128,
-                options={"batch_limit": 16},
-            )
-            self._tuning_factor = bald_val.mean().item()
+        tuning_acq = qConditionalHyperparameterInformationGain(
+            model=self.model,
+            mc_points=self.mc_points,
+            sampler=self.sampler,
+            X_pending=self.X_pending,
+            posterior_transform=self.posterior_transform,
+        )
+        tuning_samples = draw_sobol_samples(
+            n=self.num_beta_tuning_samples, bounds=self.bounds, q=q
+        )
+        # Optimize the acquisition function over the tuning samples
+        # NOTE: This is not a proper acquisition function optimization,
+        # but gradient-based optimization of the tuning factor has not been
+        #  found to be important (and is not possible due to a memory leak).
+        acq_evals = torch.cat(
+            [
+                tuning_acq(sample_split)
+                for sample_split in tuning_samples.split(split_size=16, dim=0)
+            ]
+        )
+        self._tuning_factor = acq_evals.max().item()
         self._tuning_factor_q = q
 
     @concatenate_pending_points
--- test_community/acquisition/test_bayesian_active_learning.py
+++ test_community/acquisition/test_bayesian_active_learning.py
@@ -13,6 +13,7 @@ from botorch.utils.testing import BotorchTestCase
 from botorch_community.acquisition.bayesian_active_learning import (
     qBayesianQueryByComittee,
     qBayesianVarianceReduction,
+    qConditionalHyperparameterInformationGain,
     qExpectedPredictiveInformationGain,
     qHyperparameterInformedPredictiveExploration,
     qStatisticalDistanceActiveLearning,
@@ -76,6 +77,36 @@ class TestQStatisticalDistanceActiveLearning(BotorchTestCase):
                     self.assertTrue(acq_X.shape == test_Xs[j].shape[:-2])
 
 
+class TestQConditionalHyperparameterInformationGain(BotorchTestCase):
+    def test_q_conditional_hyperparameter_information_gain(self):
+        torch.manual_seed(1)
+        tkwargs = {"device": self.device, "dtype": torch.double}
+        input_dim = 2
+        num_models = 3
+
+        model = get_fully_bayesian_model(
+            train_X=torch.rand(4, input_dim, **tkwargs),
+            train_Y=torch.rand(4, 1, **tkwargs),
+            num_models=num_models,
+            **tkwargs,
+        )
+        bounds = torch.tensor([[0.0] * input_dim, [1.0] * input_dim], **tkwargs)
+        mc_points = draw_sobol_samples(bounds=bounds, n=11, q=1).squeeze(-2)
+
+        acq = qConditionalHyperparameterInformationGain(
+            model=model, mc_points=mc_points
+        )
+        test_X = torch.rand(7, 1, input_dim, **tkwargs)
+        acq_X = acq(test_X)
+        self.assertEqual(acq_X.shape, test_X.shape[:-2])
+        self.assertTrue((acq_X >= 0).all())
+
+        test_X = torch.rand(7, 3, input_dim, **tkwargs)
+        acq_X = acq(test_X)
+        self.assertEqual(acq_X.shape, test_X.shape[:-2])
+        self.assertTrue((acq_X >= 0).all())
+
+
 class TestQExpectedPredictiveInformationGain(BotorchTestCase):
     def test_q_expected_predictive_information_gain(self):
         torch.manual_seed(1)
@@ -83,8 +114,8 @@ class TestQExpectedPredictiveInformationGain(BotorchTestCase):
         input_dim = 2
 
         model = get_fully_bayesian_model(
-            train_X=torch.rand(4, input_dim, **tkwargs),
-            train_Y=torch.rand(4, 1, **tkwargs),
+            train_X=torch.rand(0, input_dim, **tkwargs),
+            train_Y=torch.rand(0, 1, **tkwargs),
             num_models=3,
             **tkwargs,
         )
@@ -113,23 +144,21 @@ class TestQHyperparameterInformedPredictiveExploration(BotorchTestCase):
         num_models = 3
         for (
             dtype,
-            standardize_model,
             infer_noise,
         ) in product(
             (torch.float, torch.double),
-            (False, True),
             (True,),
         ):
             tkwargs["dtype"] = dtype
             input_dim = 2
-            train_X = torch.rand(4, input_dim, **tkwargs)
-            train_Y = torch.rand(4, num_objectives, **tkwargs)
+            train_X = torch.rand(0, input_dim, **tkwargs)
+            train_Y = torch.rand(0, num_objectives, **tkwargs)
 
             model = get_fully_bayesian_model(
                 train_X=train_X,
                 train_Y=train_Y,
                 num_models=num_models,
-                standardize_model=standardize_model,
+                standardize_model=False,
                 infer_noise=infer_noise,
                 **tkwargs,
             )
@@ -142,7 +171,6 @@ class TestQHyperparameterInformedPredictiveExploration(BotorchTestCase):
                 model=model,
                 mc_points=mc_points,
                 bounds=bounds,
-                beta=1.0,
             )
 
             test_Xs = [
@@ -157,20 +185,18 @@ class TestQHyperparameterInformedPredictiveExploration(BotorchTestCase):
                 self.assertTrue((acq_X > 0).all())
 
             # test beta tuning (beta=None) and re-tuning when q changes
-            for beta_tuning_method in ("sobol", "optimize"):
-                acq_tuned = qHyperparameterInformedPredictiveExploration(
-                    model=model,
-                    mc_points=mc_points,
-                    bounds=bounds,
-                    beta_tuning_method=beta_tuning_method,
-                )
-                # first forward pass computes tuning factor for q=1
-                acq_tuned(torch.rand(4, 1, input_dim, **tkwargs))
-                tuning_factor_q1 = acq_tuned._tuning_factor
-                # second forward pass with different q recomputes tuning factor
-                acq_tuned(torch.rand(4, 3, input_dim, **tkwargs))
-                tuning_factor_q3 = acq_tuned._tuning_factor
-                self.assertNotEqual(tuning_factor_q1, tuning_factor_q3)
+            acq = qHyperparameterInformedPredictiveExploration(
+                model=model,
+                mc_points=mc_points,
+                bounds=bounds,
+            )
+            # first forward pass computes tuning factor for q=1
+            acq(torch.rand(4, 1, input_dim, **tkwargs))
+            tuning_factor_q1 = acq._tuning_factor
+            # second forward pass with different q recomputes tuning factor
+            acq(torch.rand(4, 3, input_dim, **tkwargs))
+            tuning_factor_q3 = acq._tuning_factor
+            self.assertNotEqual(tuning_factor_q1, tuning_factor_q3)
 
             # test that mc_points must be 2-dimensional
             with self.assertRaises(ValueError):
